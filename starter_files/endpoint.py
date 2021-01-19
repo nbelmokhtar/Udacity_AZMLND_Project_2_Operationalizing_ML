63daf45dbda3fbc0605905702ca923ded6d9600e
Nabil BELMOKHTAR <64109508+nbelmokhtar@users.noreply.github.com>
2021-01-19 22:25:43 +0100

    Add files via upload

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,71 +3,41 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = ""
+
 # If the service is authenticated, set the key or token
-key = ''
+key = ""
 
 # Two sets of data to score, so we get two results back
-data = {"data":
-        [
-          {
-            "age": 17,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 971,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-          {
-            "age": 87,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 471,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-      ]
-    }
+data = {
+    "data": [
+        {
+            "instant": 1,
+            "date": "2013-01-01 00:00:00,000000",
+            "season": 1,
+            "yr": 0,
+            "mnth": 1,
+            "weekday": 6,
+            "weathersit": 2,
+            "temp": 0.344167,
+            "atemp": 0.363625,
+            "hum": 0.805833,
+            "windspeed": 0.160446,
+            "casual": 331,
+            "registered": 654,
+        },
+    ]
+}
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
     _f.write(input_data)
 
 # Set the content type
-headers = {'Content-Type': 'application/json'}
+headers = {"Content-Type": "application/json"}
 # If authentication is enabled, set the authorization header
-headers['Authorization'] = f'Bearer {key}'
+headers["Authorization"] = f"Bearer {key}"
 
 # Make the request and display the response
 resp = requests.post(scoring_uri, input_data, headers=headers)
 print(resp.json())
-
-
