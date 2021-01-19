a555a2cbae042e325e6e3619569f81f0a3ba7c7d
Nabil BELMOKHTAR <64109508+nbelmokhtar@users.noreply.github.com>
2021-01-19 22:46:23 +0100

    Add files via upload

--- Exercise_starter_files/logs.py
+++ Exercise_starter_files/logs.py
@@ -5,10 +5,11 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "demo-model-deploy"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+service.update(enable_app_insights=True)
 logs = service.get_logs()
 
 for line in logs.split('\n'):
